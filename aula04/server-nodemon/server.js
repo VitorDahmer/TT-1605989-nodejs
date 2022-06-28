cf388a027be8a5c9ad46247996e17f2a05fdf332
Vitor Dahmer <vitor.dahmer@hotmail.com>
2022-06-27 22:02:42 -0300

    Nodemon e pacotes

---
+++ aula04/server-nodemon/server.js
@@ -0,0 +1,12 @@
+import { createServer } from 'http'
+
+createServer((req, res) => {
+  res.writeHead(200, { 'Content-type' : 'text/html; charset=utf-8'})
+
+  
+  res.write('<h1>Bem vindo Turma Aula04</h1>')
+
+  res.end()
+}).listen(3030, () => {
+  console.log('Running server!')
+})
